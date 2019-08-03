0a4669cf8fbb9efb4bef9ec5c5f2ea27d3c6efbf
Jexeones24 <jexeones@gmail.com>
2019-08-03 11:41:17 -0400

    scrapes and builds data obj

--- data.js
+++ data.js
@@ -4,44 +4,53 @@ const cheerio = require('cheerio');
 const URL = 'https://factba.se/topic/calendar';
 
 const removeWhiteSpace = str => str.replace(/\s\s+/g, ' ');
-
 let getData = html => {
-  data = [];
   const $ = cheerio.load(html);
-  // const monthlyStats = $('.agenda-month', '#calendar_rows').text();
-  // const sanitizedMonthlyStats = removeWhiteSpace(monthlyStats).split('\t');
+
+  data = {};
+
+  const monthlyStats = $('.agenda-month', '#calendar_rows').text();
+  const sanitizedStats = removeWhiteSpace(monthlyStats).split('\t');
 
   const calendarRows = $('#calendar_rows').each((i, row) => {
-    // const array = $('td').toArray();
-
-    ////////////////
-
     const dates = $('td[class=agenda-date]').toArray();
+    const today = dates[0];
 
-    $(dates).each((i, date) => {
-      const rowspan = Number($(date).attr('rowspan'));
-      const dateObj = $(date)
+    const rowspan = $(today).attr('rowspan');
+    const times = $('.agenda-time, .timefirst').toArray();
+    const agendaEvents = $('td[class=agenda-events]').toArray();
+
+    const todaysTimes = times.slice(0, rowspan);
+    const todaysEvents = agendaEvents.slice(0, rowspan);
+
+    let eventList = [];
+    $(todaysTimes).each((i, time) => {
+      const eventTime = $(time)
+        .text()
+        .replace(/[\n\t\r]/g,"")
+        .split(', ')
+        .join();
+      const todaysEvent = todaysEvents[i];
+      const event = $(todaysEvent)
         .text()
         .replace(/[\n\t\r]/g,"")
         .split(', ')
         .join();
 
-      // grab sibling trs X rowspan
-
-      data.push({
-        date: dateObj,
-        rowspan,
-      });
+      eventList.push({ eventTime, event });
     });
+
+    const fullDate = $(today)
+      .text()
+      .replace(/[\n\t\r]/g,"")
+      .split(', ')
+      .join();
+
+    data['monthlyStats'] = sanitizedStats;
+    data['todaysDate'] = fullDate;
+    data['events'] = eventList;
+
     console.log(data);
-  });
-
-  const agendaTimes = $('.agenda-time', '#calendar_rows').each((i, time) => {
-    const agendaTime = $(time).text().replace(/[\n\t\r]/g,"");
-    const agendaEvent = $(time).siblings('td').text().replace(/[\n\t\r]/g,"");
-
-    // console.log('TIME:', agendaTime + '\n');
-    // console.log('EVENT:', agendaEvent + '\n')
   });
 };
 
